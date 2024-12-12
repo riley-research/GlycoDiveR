e3fc4474d4da8e8d21b7a34deb38889bb1c359f5
Tim S Veth <tveth@uw.edu>
2024-12-12 10:35:02 -0800

    Update test case

--- R/MSFraggerImporter.R
+++ R/MSFraggerImporter.R
@@ -36,19 +36,19 @@ MSFraggerImporter <- function(path, annotation, fastaPath, peptideScoreCutoff, g
                   #                  peptideScoreCutoff = 40,
                   #                  glycanScoreCutoff = -100)
 
-testcase <- subset(testdf$PSMTable, UniprotIDs == "P09871,F8WCZ6" & Run == "240419_ES_R00006_MG_c00001_SA_NPB_1_i1")
-testcase <- testcase %>%
-  dplyr::filter(!is.na(AssignedModifications) & AssignedModifications != "") %>%
-  tidyr::separate_rows(AssignedModifications, sep = ",") %>%
-  dplyr::filter(!grepl("C\\(57.0215\\)", AssignedModifications)) %>%
-  dplyr::rowwise() %>%
-  dplyr::mutate(PeptidePTMLocalization = as.numeric(regmatches(AssignedModifications, gregexpr("^[0-9]+", AssignedModifications))[[1]]),
-                ProteinPTMLocalization = PeptidePTMLocalization + ProteinStart,
-                ModificationSite = sub(".*([A-Za-z])\\(.*", "\\1", AssignedModifications),
-                ModificationID = paste0(ModificationSite,ProteinPTMLocalization))
+#testcase <- subset(testdf$PSMTable, UniprotIDs == "P09871,F8WCZ6" & Run == "240419_ES_R00006_MG_c00001_SA_NPB_1_i1")
+#testcase <- testcase %>%
+#  dplyr::filter(!is.na(AssignedModifications) & AssignedModifications != "") %>%
+#  tidyr::separate_rows(AssignedModifications, sep = ",") %>%
+#  dplyr::filter(!grepl("C\\(57.0215\\)", AssignedModifications)) %>%
+#  dplyr::rowwise() %>%
+#  dplyr::mutate(PeptidePTMLocalization = as.numeric(regmatches(AssignedModifications, gregexpr("^[0-9]+", AssignedModifications))[[1]]),
+#                ProteinPTMLocalization = PeptidePTMLocalization + ProteinStart,
+#                ModificationSite = sub(".*([A-Za-z])\\(.*", "\\1", AssignedModifications),
+#                ModificationID = paste0(ModificationSite,ProteinPTMLocalization))
 
-ggplot2::ggplot(testcase) +
-  geom_line(data = data.frame(x = seq(1,850), y = 1), aes(x = x, y = y), size = 2) +
-  geom_point(data = testcase, aes(x= ProteinPTMLocalization, y = 1), color = "white", fill = "red") +
-  ggrepel::geom_label_repel(data = distinct(testcase[c("ProteinPTMLocalization", "ModificationID")]), aes(x =ProteinPTMLocalization, y = 1, label = ModificationID)) +
-  theme_void()
+#ggplot2::ggplot(testcase) +
+#  geom_line(data = data.frame(x = seq(1,850), y = 1), aes(x = x, y = y), size = 2) +
+#  geom_point(data = testcase, aes(x= ProteinPTMLocalization, y = 1), color = "white", fill = "red") +
+#  ggrepel::geom_label_repel(data = distinct(testcase[c("ProteinPTMLocalization", "ModificationID")]), aes(x =ProteinPTMLocalization, y = 1, label = ModificationID)) +
+#  theme_void()
