3189235ca6b0cd8c921f2d6ed9839c922e11c50e
Tim <tim_veth@hotmail.com>
2025-01-07 13:49:14 +0100

    Updated legend order

--- R/PlotGlycansPerSite.R
+++ R/PlotGlycansPerSite.R
@@ -13,7 +13,7 @@ PlotGlycansPerSite <- function(input){
     dplyr::ungroup()
 
   df$GlycansPerSite <- sapply(df$GlycansPerSite, function(x) ifelse(x > 9, ">10", toString(x)))
-  df$GlycansPerSite <- factor(df$GlycansPerSite, levels = c("1", "2", "3", "4", "5", "6", "7", "8", "9", ">10"))
+  df$GlycansPerSite <- factor(df$GlycansPerSite, levels = rev(c("1", "2", "3", "4", "5", "6", "7", "8", "9", ">10")))
 
   df <- df %>%
     dplyr::group_by(GlycansPerSite) %>%
@@ -29,7 +29,7 @@ PlotGlycansPerSite <- function(input){
   p <- ggplot2::ggplot(df, aes(x="", y=Count, fill=GlycansPerSite)) +
     ggplot2::geom_bar(stat="identity", width=1, color= "black") +
     ggplot2::coord_polar("y", start=0) +
-    ggplot2::scale_fill_manual(values = colorScheme) +
+    ggplot2::scale_fill_manual(values = colorScheme, guide = guide_legend(reverse = TRUE)) +
     ggplot2::geom_text(aes(label = Count),
                        position = position_stack(vjust = 0.5)) +
     ggplot2::labs(fill = "Glycans\nper site")
