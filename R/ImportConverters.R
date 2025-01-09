53bf7f6e17f1e84c3c104584e80e666f45cd652b
Tim <tim_veth@hotmail.com>
2025-01-09 20:29:26 +0100

    Added completeness heatmap

--- R/ImportConverters.R
+++ R/ImportConverters.R
@@ -1,35 +1,35 @@
 #source(here::here("R/GlycoDiveRUtils.R"))
 
 MSFraggerConverter <- function(unfiltereddf, annotationdf, fastaPath){
-  message("\033[30m[", base::substr(Sys.time(), 1, 16), "] INFO: Now starting import.\033[0m")
+  fmessage("Now starting import.")
   filtereddf <- data.frame(ID = seq(1:nrow(unfiltereddf)))
   existingCols <- unique(names(unfiltereddf))
 
   if("Run" %in% existingCols){
     filtereddf <- cbind(filtereddf, Run = as.character(unfiltereddf$Run))
-    message("\033[30m[", base::substr(Sys.time(), 1, 16), "] INFO: Successfully imported Run column.\033[0m")
+    fmessage("Successfully imported Run column.")
   }
   else{stop("The column Run was not found in the input dataframe.")}
 
   if("Modified.Peptide" %in% existingCols){
     filtereddf$ModifiedPeptide <- apply(unfiltereddf[,c("Peptide", "Modified.Peptide")], 1, function(x) GetPeptide(pep = x[1], modpep = x[2]))
-    message("\033[30m[", base::substr(Sys.time(), 1, 16), "] INFO: Successfully imported Modified Peptide column.\033[0m")}
+    fmessage("Successfully imported Modified Peptide column.")}
   else{stop("The column Modified.Peptide was not found in the input dataframe.")}
 
   if("Intensity" %in% existingCols) {
     filtereddf <- cbind(filtereddf, Intensity = as.numeric(unfiltereddf$Intensity))
-    message("\033[30m[", base::substr(Sys.time(), 1, 16), "] INFO: Successfully imported Intensity column.\033[0m")}
+    fmessage("Successfully imported Intensity column.")}
   else{filtereddf <- cbind(filtereddf, Intensity = as.numeric(NA))
        warning("Intensity column not found. Filled with NA.")}
 
   if ("Hyperscore" %in% existingCols) {
     filtereddf <- cbind(filtereddf, PSMScore = as.double(unfiltereddf$Hyperscore))
-    message("\033[30m[", base::substr(Sys.time(), 1, 16), "] INFO: Successfully imported Hyperscore column and renamed to PSMScore.\033[0m")}
+    fmessage("Successfully imported Hyperscore column and renamed to PSMScore.")}
   else{stop("The column Hyperscore was not found in the input dataframe.")}
 
   if ("Assigned.Modifications" %in% existingCols) {
     filtereddf <- cbind(filtereddf, AssignedModifications = as.character(unfiltereddf$Assigned.Modifications))
-    message("\033[30m[", base::substr(Sys.time(), 1, 16), "] INFO: Successfully imported Assigned Modifications column.\033[0m")}
+    fmessage("Successfully imported Assigned Modifications column.")}
   else {stop("The column Assigned.Modifications was not found in the input dataframe.")}
 
   if ("Total.Glycan.Composition" %in% existingCols) {
@@ -40,18 +40,18 @@ MSFraggerConverter <- function(unfiltereddf, annotationdf, fastaPath){
 
   if ("Glycan.q.value" %in% existingCols) {
     filtereddf <- cbind(filtereddf, GlycanQValue = as.double(unfiltereddf$Glycan.q.value))
-    message("\033[30m[", base::substr(Sys.time(), 1, 16), "] INFO: Successfully imported Glycan q Value column.\033[0m")}
+    fmessage("Successfully imported Glycan q Value column.")}
   else {stop("The column Glycan.q.value was not found in the input dataframe.")}
 
   if ("Is.Unique" %in% existingCols) {
     filtereddf <- cbind(filtereddf, IsUnique = as.logical(unfiltereddf$Is.Unique))
-    message("\033[30m[", base::substr(Sys.time(), 1, 16), "] INFO: Successfully imported Is Unique column.\033[0m")}
+    fmessage("Successfully imported Is Unique column.")}
   else {stop("The column Is.Unique was not found in the input dataframe.")}
 
   if ("Protein.ID" %in% existingCols) {
     if ("Mapped.Proteins" %in% existingCols) {
       filtereddf$UniprotIDs <- apply(unfiltereddf[, c("Protein.ID", "Mapped.Proteins")], 1, function(x) CombineProtCols(x))
-      message("\033[30m[", base::substr(Sys.time(), 1, 16), "] INFO: Successfully imported and merged Protein ID and Mapped protein columns.\033[0m")
+      fmessage("Successfully imported and merged Protein ID and Mapped protein columns.")
     } else {
       filtereddf <- cbind(filtereddf, UniprotIDs = as.character(unfiltereddf$Protein.ID))
       warning("No Mapped.Proteins column found, only taking from Protein ID column.")
@@ -62,7 +62,7 @@ MSFraggerConverter <- function(unfiltereddf, annotationdf, fastaPath){
   if ("Gene" %in% existingCols) {
     if("Mapped.Genes" %in% existingCols){
       filtereddf$Genes <- apply(unfiltereddf[, c("Gene", "Mapped.Genes")], 1, function(x) CombineTwoCols(x))
-      message("\033[30m[", base::substr(Sys.time(), 1, 16), "] INFO: Successfully imported and merged Gene and Mapped Genes columns.\033[0m")
+      fmessage("Successfully imported and merged Gene and Mapped Genes columns.")
     }else {
       filtereddf <- cbind(filtereddf, UniprotIDs = as.character(unfiltereddf$Protein.ID))
       warning("No Mapped.Genes columns found, only taking from Gene column")}
@@ -103,7 +103,7 @@ MSFraggerConverter <- function(unfiltereddf, annotationdf, fastaPath){
   filtereddf$GlycanType <- apply(filtereddf[,c("AssignedModifications", "TotalGlycanComposition")], 1, function(x) GlycanComptToGlycanType(mod = x[1], glycanComp = x[2]))
   filtereddf <- filtereddf %>%
     dplyr::mutate(GlycanType = sapply(GlycanType, toString))
-  message("\033[30m[", base::substr(Sys.time(), 1, 16), "] INFO: Successfully added GlycanType column.\033[0m")
+  fmessage("Successfully added GlycanType column.")
 
   filtereddf <- filtereddf %>%
     dplyr::left_join(annotationdf, by = "Run")
---
+++ R/PlotCompletenessHeatmap.R
@@ -0,0 +1,50 @@
+PlotCompletenessHeatmap <- function(input, grouping = "peptide", peptideType  = "glyco"){
+  input <- FilterForCutoffs(input)
+
+  if(grouping == "peptide"){
+    df <- GetMeanTechReps(input$PSMTable)
+  }
+
+  df$GlycanType <- sapply(df$GlycanType, function(x) gsub(", |NonGlyco|Unmodified", "", x))
+
+  if(peptideType == "glyco"){
+    df <- subset(df, GlycanType != "")
+  }
+
+  df <- df[,c("Alias", "GlycanType", "Condition", "ModifiedPeptide", "Run")] %>%
+    tidyr::pivot_wider(names_from = Alias, values_from = Run)
+
+  df <- df %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(dplyr::across(-c(ModifiedPeptide, GlycanType), ~ ifelse(!is.na(.), 1, 0)))
+
+  #Get the matrix and in the right column order
+  mtrx <- data.matrix(df[,4:ncol(df)])
+  rownames(mtrx) <- df$ModifiedPeptide
+
+  mtrx <- mtrx[,levels(input$PTMTable$Alias), drop = FALSE]
+
+  #Get heatmap annotation, color, and legend
+  colH <- setNames(colorScheme[1:length(unique(df$GlycanType))], unique(df$GlycanType))
+
+  row_ha = ComplexHeatmap::rowAnnotation(Glycan = df$GlycanType, show_legend = FALSE,
+                                         col = list(Glycan = colH))
+
+  col_fun = circlize::colorRamp2(c(0,1), c("lightgrey", "darkgreen"))
+  col_fun(seq(-3, 3))
+
+  lgd = ComplexHeatmap::Legend(col_fun = col_fun, title = "", at = c(0, 1),
+               labels = c("Missing", "Identified"))
+
+  #Now for clustering the columns
+  colCluster <- sapply(colnames(mtrx), function(x) subset(input$annotation, Alias == x)$Condition[1])
+
+  ht <- ComplexHeatmap::Heatmap(mtrx, show_row_names = FALSE, cluster_rows = FALSE,
+                               cluster_columns = FALSE, left_annotation = row_ha,
+                               row_split = df$GlycanType, use_raster = FALSE,
+                               cluster_row_slices = TRUE, border = TRUE,
+                               col = col_fun, show_heatmap_legend = FALSE,
+                               column_split = colCluster)
+
+  print(ComplexHeatmap::draw(ht, annotation_legend_list = list(lgd)))
+}
