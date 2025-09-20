aeb62bba0cae7a896547f7ca8a646ece6fee6fa5
Tim <tveth@uw.edu>
2025-09-19 17:19:56 -0700

    Improved code

--- R/PlotGlycositesVsGlycans.R
+++ R/PlotGlycositesVsGlycans.R
@@ -10,24 +10,26 @@ PlotGlycositesVsGlycans <- function(input, whichAlias = NULL,
 
   #Prepare data
   df <- input$PTMTable %>%
-    dplyr::filter(!is.na(.data$TotalGlycanComposition) & .data$TotalGlycanComposition != "") %>%
-    dplyr::distinct(.data$UniprotIDs, .data$TotalGlycanComposition, .data$ModificationID, .keep_all = TRUE) %>%
-    dplyr::summarise(.by = c(.data$UniprotIDs, .data$Genes, .data$NumberOfNSites),
-                     count = dplyr::n())
+    dplyr::filter(!is.na(.data$TotalGlycanComposition) & .data$TotalGlycanComposition != "" &
+                    GlycanType != "NonGlyco") %>%
+    dplyr::mutate(.by = .data$UniprotIDs,
+                  count = dplyr::n_distinct(.data$TotalGlycanComposition)) %>%
+    dplyr::summarise(.by = c(.data$UniprotIDs, .data$count, .data$Genes),
+                             numberOfGlycosites = dplyr::n_distinct(.data$ModificationID))
 
   #Get labels
   df <- df %>%
-    dplyr::mutate(label = dplyr::case_when(.data$NumberOfNSites > labelGlycositeCutoff &
+    dplyr::mutate(label = dplyr::case_when(.data$numberOfGlycosites > labelGlycositeCutoff &
                                       .data$count > labelGlycanCutoff ~ .data$Genes,
                                     TRUE ~ NA))
   # Plot
   df %>%
-    ggplot2::ggplot(mapping = ggplot2::aes(x=.data$NumberOfNSites, y =.data$count)) +
+    ggplot2::ggplot(mapping = ggplot2::aes(x=.data$numberOfGlycosites, y =.data$count)) +
     ggplot2::geom_abline(slope = 1, intercept = 0, color = "grey50", linetype = "dashed") +
     ggplot2::geom_point(color = "#7b5799", alpha = 0.5) +
     ggrepel::geom_label_repel(ggplot2::aes(label = .data$label), max.overlaps = 10,
                               fill = NA, label.size = NA) +
-    ggplot2::annotate("text", x= 0.95 * max(df$NumberOfNSites), y= 1.1 * max(df$NumberOfNSites),
+    ggplot2::annotate("text", x= 0.95 * max(df$numberOfGlycosites), y= 1.1 * max(df$numberOfGlycosites),
                       label= "y=x", color = "grey30") +
     ggplot2::labs(x = "Number of glycosites", y = "Number of glycans")
 
