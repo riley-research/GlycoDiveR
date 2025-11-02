ccbd07bbba4efe05d6fc7ea3572e199c10a8d447
Tim <tveth@uw.edu>
2025-11-02 08:45:30 -0800

    Compute theoretical number of glycoforms

---
+++ R/ComputeNumberOfGlycoforms.R
@@ -0,0 +1,34 @@
+#' ComputeNumberOfGlycoforms
+#'
+#' @param input Formatted data
+#' @param UniprotID The UniprotID as in the UniprotIDs column
+#' @param whichAlias provide a vector of Aliases to only select these aliases
+#' for plotting
+#'
+#' @returns The number of potential proteoforms
+#' @export
+#'
+#' @examples \dontrun{ComputeNumberOfGlycoforms(mydata, "Q9NZQ7")}
+ComputeNumberOfGlycoforms <- function(input, UniprotID, whichAlias = NULL){
+  input <- FilterForCutoffs(input)
+
+  df <- input$PTMTable %>%
+    dplyr::filter(.data$GlycanType != "NonGlyco")
+
+  if(!is.null(whichAlias)){
+    df <- df %>%
+      dplyr::filter(.data$Alias %in% whichAlias)
+  }
+
+  df <- df %>%
+    dplyr::filter(.data$UniprotIDs == UniprotID) %>%
+    dplyr::distinct(.data$ModificationID, .data$TotalGlycanComposition) %>%
+    dplyr::summarise(.by = .data$ModificationID, count = dplyr::n())
+
+  if(nrow(df) == 0 | sum(df$count, na.rm = TRUE) == 0){
+    return("None found")
+  }else{
+    rslt = prod(df$count, na.rm = TRUE)
+    return(rslt)
+  }
+}
