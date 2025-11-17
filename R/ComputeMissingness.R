d0d4c54784267a214d0e5612b95034731a32a9f2
Tim <tveth@uw.edu>
2025-11-17 12:53:45 -0800

    Added functionality to delete NA values

--- R/ComputeMissingness.R
+++ R/ComputeMissingness.R
@@ -10,6 +10,8 @@
 #' with a ModifiedPeptide peptide column, or a vector with the ModifiedPeptide sequences
 #' that you want to keep. Inputted data with the comparison importer functions is
 #' directly usable, also after filtering using the FilterComparison function.
+#' @param removeAllNA Remove peptides with only NA values. This can, for example
+#' happen if you upload abundance values using Perseus or MSstats.
 #' @param whichAlias provide a vector of Aliases to only select these aliases
 #' for plotting
 #' @param silent silence printed information (default = FALSE)
@@ -18,10 +20,15 @@
 #' @export
 #'
 #' @examples \dontrun{ComputeMissingness(mydata, type = "glyco")}
-ComputeMissingness <- function(input, type = "glyco", whichPeptide = NA,
+ComputeMissingness <- function(input, type = "glyco", removeAllNA = TRUE,
+                               whichPeptide = NA,
                                     whichAlias = NULL, silent = FALSE){
   input <- FilterForCutoffs(input, silent)
   df <- FilterForPeptides(input$PSMTable, whichPeptide)
+  if(removeAllNA){
+    df <- df %>%
+      dplyr::filter(!is.na(.data$Intensity))
+  }
   if(!is.null(whichAlias)){
     df <- df %>%
       dplyr::filter(.data$Alias %in% whichAlias)
@@ -31,7 +38,7 @@ ComputeMissingness <- function(input, type = "glyco", whichPeptide = NA,
     glycoPSMTypes <- c("Sialyl", "Complex/Hybrid", "Sialyl+Fucose",
                        "Fucose", "Truncated", "High Mannose", "Paucimannose",
                        "OGlycan")
-    df <- input$PSMTable %>%
+    df <- df %>%
       dplyr::mutate(PSMType = dplyr::case_when(stringr::str_count(.data$GlycanType, paste(glycoPSMTypes, collapse = "|")) > 1 &
                                                  stringr::str_count(.data$GlycanType, "Sialyl") == 2 &
                                                  grepl("Sialyl+Fucose", .data$GlycanType) == 1 ~ "Multi",
